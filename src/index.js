8fbcf03d2ff31692ffc710b0c29de3e1cb68abe9
VuDej <93735137+VuDej@users.noreply.github.com>
2022-01-19 14:41:43 +0100

    reverseString passed

--- src/index.js
+++ src/index.js
@@ -5,34 +5,13 @@ const stringLength = (string) => {
   return string.length;
 }
 
-const stringLength = (string) => {
-  if (string.length < 1 || string.length > 10 ) {
-    throw new Error( 'String should has length between 1 and 10 characters');
-  }
-  return string.length;
-}
+const reverseString = (string) => string.split('').reverse().join('');
 
-const stringLength = (string) => {
-  if (string.length < 1 || string.length > 10 ) {
-    throw new Error( 'String should has length between 1 and 10 characters');
-  }
-  return string.length;
-}
 
-const stringLength = (string) => {
-  if (string.length < 1 || string.length > 10 ) {
-    throw new Error( 'String should has length between 1 and 10 characters');
-  }
-  return string.length;
-}
 
-const stringLength = (string) => {
-  if (string.length < 1 || string.length > 10 ) {
-    throw new Error( 'String should has length between 1 and 10 characters');
-  }
-  return string.length;
-}
+
 
 
 
 exports.stringLength = stringLength;
+exports.reverseString = reverseString;
